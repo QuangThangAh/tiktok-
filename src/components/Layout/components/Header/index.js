e8af3861bcf1d7eb04fd40498563d4b04d6a172e
nqt152008 <thaibinhfc152008@gmail.com>
2023-12-29 08:23:36 +0700

    Bài 63.

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,10 +1,31 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+import images from '~/assets/images';
 import styles from './Header.module.scss';
 
 const cx = classNames.bind(styles);
+
 function Header() {
     return <header className={cx('wrapper')}>
-        <div className={cx('content')}></div>
+        <div className={cx('content')}>
+            <img src={images.logo} alt="Tik-tok logo" />
+            <div className={cx('search')}>
+                <input type="text"
+                    placeholder='Tìm kiếm'
+                    spellCheck={false}
+                />
+                <button className={cx('clear')}>
+                    {<FontAwesomeIcon icon={faCircleXmark} />}
+                </button>
+                {<FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+                <button className={cx('search-btn')}>
+                    {<FontAwesomeIcon icon={faMagnifyingGlass} />}
+                </button>
+            </div>
+            <div className={cx('action')}></div>
+        </div>
     </header>;
 }
 
