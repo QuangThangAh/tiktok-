7a8b04078db3a672be29ce90825e0602399eace6
Quang Thang <nqt152008@gmail.com>
2023-12-30 14:59:59 +0700

    Header UI

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,8 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
 
 import images from '~/assets/images';
 import styles from './Header.module.scss';
@@ -12,14 +14,23 @@ function Header() {
         <div className={cx('content')}>
             <img src={images.logo} alt="Tik-tok logo" />
             <div className={cx('search')}>
-                <input type="text"
-                    placeholder='Tìm kiếm'
-                    spellCheck={false}
-                />
+                <Tippy render={attrs => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        Kết quả tìm kiếm
+                    </div>
+                )}
+                >
+                    <input type="text"
+                        placeholder='Tìm kiếm'
+                        spellCheck={false}
+                    />
+
+                </Tippy>
                 <button className={cx('clear')}>
                     {<FontAwesomeIcon icon={faCircleXmark} />}
                 </button>
                 {<FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+
                 <button className={cx('search-btn')}>
                     {<FontAwesomeIcon icon={faMagnifyingGlass} />}
                 </button>
