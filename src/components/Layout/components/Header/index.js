49077a6d019539b29bcc863da201b69bd5bde618
nqt152008 <thaibinhfc152008@gmail.com>
2024-01-02 03:49:24 +0700

    UI-header 2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,24 +1,49 @@
+//Library import
 import classNames from 'classnames/bind';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
-
+//Local import
 import images from '~/assets/images';
 import styles from './Header.module.scss';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItems';
 
 const cx = classNames.bind(styles);
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([])
+
+    useEffect(() => {
+        setTimeout(() => { setSearchResult([]) }, 2000)
+    }
+        , [])
+
+
+
     return <header className={cx('wrapper')}>
         <div className={cx('content')}>
             <img src={images.logo} alt="Tik-tok logo" />
             <div className={cx('search')}>
-                <Tippy render={attrs => (
-                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                        Kết quả tìm kiếm
-                    </div>
-                )}
+                <Tippy
+                    interactive
+                    placement="bottom"
+                    visible={searchResult.length > 0}
+                    render={attrs => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h3 className={cx('search-title')}>
+                                    Accounts
+                                </h3>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
                 >
                     <input type="text"
                         placeholder='Tìm kiếm'
